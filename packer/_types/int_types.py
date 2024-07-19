2ce13fc016a3fb1b773eea5e2c5fd7caa8aab389
biggus-developerus <74741815+biggus-developerus@users.noreply.github.com>
2024-07-19 13:22:04 +0400

    Create int_types.py

---
+++ packer/_types/int_types.py
@@ -0,0 +1,43 @@
+__all__ = ("Int32", "Int16", "Int8")
+
+import sys
+
+from .base import (
+    TypeDescriptor,
+)
+
+
+class Int32(TypeDescriptor):
+    __data_size__: int = 4
+
+    @classmethod
+    def pack(cls, val: int) -> bytes:
+        return val.to_bytes(cls.__data_size__, sys.byteorder)
+
+    @classmethod
+    def unpack(cls, data: bytearray) -> tuple[int, int]:
+        return int.from_bytes(data[: cls.__data_size__], sys.byteorder)
+
+
+class Int16(TypeDescriptor):
+    __data_size__: int = 2
+
+    @classmethod
+    def pack(cls, val: int) -> bytes:
+        return val.to_bytes(cls.__data_size__, sys.byteorder)
+
+    @classmethod
+    def unpack(cls, data: bytearray) -> tuple[int, int]:
+        return int.from_bytes(data[: cls.__data_size__], sys.byteorder)
+
+
+class Int8(TypeDescriptor):
+    __data_size__: int = 1
+
+    @classmethod
+    def pack(cls, val: int) -> bytes:
+        return val.to_bytes(cls.__data_size__, sys.byteorder)
+
+    @classmethod
+    def unpack(cls, data: bytearray) -> tuple[int, int]:
+        return int.from_bytes(data[: cls.__data_size__], sys.byteorder)
