00bfb80cf4081d1e990b03b4ca60babe720f2f0d
biggus-developerus <74741815+biggus-developerus@users.noreply.github.com>
2024-09-24 19:29:56 +0400

    int64

--- packer/_types/int_types.py
+++ packer/_types/int_types.py
@@ -1,4 +1,4 @@
-__all__ = ("Int32", "Int16", "Int8")
+__all__ = ("Int64", "Int32", "Int16", "Int8")
 
 import sys
 
@@ -16,6 +16,18 @@ class Test:
 # Where the False denotes whether it's a signed or unsigned int.
 # This could be done with metaclasses and overriding __getitem__ 👍👍
 
+class Int64(TypeDescriptor):
+    __data_size__: int = 8
+
+    @classmethod
+    def pack(cls, val: int) -> bytes:
+        return val.to_bytes(cls.__data_size__, sys.byteorder, signed=True)
+
+    @classmethod
+    def unpack(cls, data: bytearray) -> tuple[int, int]:
+        return cls.__data_size__, int.from_bytes(
+            data[: cls.__data_size__], sys.byteorder, signed=True
+        )
 
 class Int32(TypeDescriptor):
     __data_size__: int = 4
