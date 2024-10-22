6dd0034ea3d53498122c632ad2b8bc69accced7c
biggus-developerus <74741815+biggus-developerus@users.noreply.github.com>
2024-10-22 17:47:06 +0400

    Update base.py

--- packer/pack_types/base.py
+++ packer/pack_types/base.py
@@ -3,7 +3,6 @@ __all__ = (
     "OptionalPack",
 )
 
-from enum import Enum
 from typing import (
     Annotated,
     TypeVar,
@@ -12,12 +11,7 @@ from typing import (
 T = TypeVar("T")
 
 
-class PackType(Enum):
-    NON_OPTIONAL = 0
-    OPTIONAL = 1
+type Pack[T] = Annotated[T, ""]
+type OptionalPack[T] = Annotated[T, ""]
 
-
-type Pack[T] = Annotated[T, PackType.NON_OPTIONAL]
-type OptionalPack[T] = Annotated[T, PackType.OPTIONAL]
-
-# TODO: ConditionalPack?!
+# TODO: ConditionalPack & RuntimePack (where sizes are determined in runtime (length prefixed data))
