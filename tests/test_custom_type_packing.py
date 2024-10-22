3252fcbed9bfe994142d32ac7b407d82c58ac43c
biggus-developerus <74741815+biggus-developerus@users.noreply.github.com>
2024-10-22 09:49:59 +0400

    reformat

--- tests/test_custom_type_packing.py
+++ tests/test_custom_type_packing.py
@@ -1,14 +1,17 @@
 import dataclasses
-
 from enum import IntEnum
 
-from packer.protocols import TypeDescriptor
 from packer import *
+from packer.protocols import (
+    TypeDescriptor,
+)
+
 
 class FileTypes(IntEnum):
     UNK = 0
     TXT = 1
     BIN = 2
+
 
 class _FileType(TypeDescriptor):
     _size: int = 1
@@ -19,9 +22,11 @@ class _FileType(TypeDescriptor):
 
     @classmethod
     def unpack(cls, data: bytes) -> FileTypes:
-        return FileTypes(int.from_bytes(data[:cls._size], "little"))
-    
-FileType = _FileType | FileTypes # to get the typehint back
+        return FileTypes(int.from_bytes(data[: cls._size], "little"))
+
+
+FileType = _FileType | FileTypes  # to get the typehint back
+
 
 def test_custom_type_packing() -> None:
     @packable
@@ -31,11 +36,15 @@ def test_custom_type_packing() -> None:
         file_data: Pack[AllData]
 
     assert File(FileTypes.TXT, b"hello!").pack() == b"\x01hello!"
-    
+
     f = File(FileTypes.UNK, b"")
 
-    f.unpack(File(FileTypes.BIN, bytearray([3]*5)).pack())
-    assert f.file_type == FileTypes.BIN and len(f.file_data) == len(bytearray([3]*5)) and f.file_data == bytearray([3]*5)
+    f.unpack(File(FileTypes.BIN, bytearray([3] * 5)).pack())
+    assert (
+        f.file_type == FileTypes.BIN
+        and len(f.file_data) == len(bytearray([3] * 5))
+        and f.file_data == bytearray([3] * 5)
+    )
 
 
 if __name__ == "__main__":
