e2af3bbbf79c895933251a8e18ba6504e6fe696a
biggus-developerus <74741815+biggus-developerus@users.noreply.github.com>
2025-02-26 14:09:39 +0400

    rewrite

--- packer/__init__.py
+++
@@ -1,3 +0,0 @@
-from .exceptions import *
-from .pack_types import *
-from .packer import *
--- packer/pack_types/__init__.py
+++
@@ -1,4 +0,0 @@
-from .base import *
-from .other import *
-from .pack_types import *
-from .size_defined_types import *
